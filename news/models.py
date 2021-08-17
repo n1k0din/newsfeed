9561cc688f7b2ae5c62862ec13ebcb7c58f67bf0
n1k0din <nik726@gmail.com>
2021-08-17 16:39:34 +0700

    Add newspost model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsPost(models.Model):
+    title = models.CharField('Заголовок', max_length=200)
+    text = models.TextField('Полный текст')
+    published_at = models.DateTimeField('Дата и время публикации')
+    image = models.ImageField('Картинка', blank=True, upload_to='images')
