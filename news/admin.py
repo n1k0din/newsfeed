9ef83b860c8636e4b605dc8b3d2bde59f415e8fe
n1k0din <nik726@gmail.com>
2021-08-18 10:15:04 +0700

    Add photo admin previews to post admin

--- news/admin.py
+++ news/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from .models import NewsPost, Photo
+
+
+class PhotoInline(admin.TabularInline):
+    model = Photo
+
+    readonly_fields = ['image_preview']
+    fields = ['image', 'image_preview', 'sort_index']
+
+    def image_preview(self, obj, width=200):
+        return format_html('<img src={} width={}/>', obj.image.url, width)
 
 
 class NewsPostAdmin(admin.ModelAdmin):
@@ -9,6 +20,9 @@ class NewsPostAdmin(admin.ModelAdmin):
             'published_at',
         ]
 
+    inlines = [
+        PhotoInline,
+    ]
+
 
 admin.site.register(NewsPost, NewsPostAdmin)
-admin.site.register(Photo)
