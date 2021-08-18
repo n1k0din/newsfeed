91ea36c9ac6a8b8c21814bf8366aefceb2c9846c
n1k0din <nik726@gmail.com>
2021-08-18 10:29:06 +0700

    Add drag'n'drop images sort

--- news/admin.py
+++ news/admin.py
@@ -1,17 +1,18 @@
+from adminsortable2.admin import SortableInlineAdminMixin
 from django.contrib import admin
 from django.utils.html import format_html
 
 from .models import NewsPost, Photo
 
 
-class PhotoInline(admin.TabularInline):
+class PhotoInline(SortableInlineAdminMixin, admin.TabularInline):
     model = Photo
 
     readonly_fields = ['image_preview']
     fields = ['image', 'image_preview', 'sort_index']
 
     def image_preview(self, obj, width=200):
-        return format_html('<img src={} width={}/>', obj.image.url, width)
+            return format_html('<img src={} width={}/>', obj.image.url, width)        
 
 
 class NewsPostAdmin(admin.ModelAdmin):
--- newsfeed/settings.py
+++ newsfeed/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
+    'adminsortable2',
 ]
 
 MIDDLEWARE = [
